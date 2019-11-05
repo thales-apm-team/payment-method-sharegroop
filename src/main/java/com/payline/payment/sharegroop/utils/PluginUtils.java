1758b7b5b5e8174f688def5db6e96c5076e941ba
splanard-thales <sebastien.planard@mythalesgroup.io>
2019-11-05 18:15:37 +0100

    Mise à jour de la méthode PluginUtils#requestToString avec la dernière version de Natixis + ajout des TU associés (couverture de code gratuite)

--- src/main/java/com/payline/payment/sharegroop/utils/PluginUtils.java
+++ src/main/java/com/payline/payment/sharegroop/utils/PluginUtils.java
@@ -2,10 +2,15 @@ package com.payline.payment.sharegroop.utils;
 
 import org.apache.http.Header;
 import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PluginUtils {
 
@@ -50,6 +55,16 @@ public class PluginUtils {
             }
         }
         str += String.join(ln, strHeaders);
+
+        if( httpRequest instanceof HttpPost){
+            try {
+                str += ln + new BufferedReader(new InputStreamReader(((HttpPost)httpRequest).getEntity().getContent()))
+                        .lines()
+                        .collect(Collectors.joining(ln));
+            } catch (IOException e) {
+                str += ln + "<< Error retrieving request body >>";
+            }
+        }
 
         return str;
     }

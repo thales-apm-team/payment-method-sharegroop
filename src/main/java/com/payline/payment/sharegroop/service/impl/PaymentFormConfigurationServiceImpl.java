fe027ba7c5ad00c3f535a88b6bf6ed6692a387a4
splanard-thales <sebastien.planard@mythalesgroup.io>
2019-11-07 18:20:05 +0100

    Ajout de TODO suite à l'analyse de GooglePay

--- src/main/java/com/payline/payment/sharegroop/service/impl/PaymentFormConfigurationServiceImpl.java
+++ src/main/java/com/payline/payment/sharegroop/service/impl/PaymentFormConfigurationServiceImpl.java
@@ -8,8 +8,16 @@ public class PaymentFormConfigurationServiceImpl extends LogoPaymentFormConfigur
     @Override
     public PaymentFormConfigurationResponse getPaymentFormConfiguration(PaymentFormConfigurationRequest paymentFormConfigurationRequest) {
 
-        /* TODO: voir si on a besoin d'implémenter cette méthode...
-        Charge-t-on le JS ici ou dans le PaymentService ? (sachant qu'on a besoin de données de paiement)
+        /* TODO: c'est probablement ici qu'on va construire le JS
+        (insérer les bonnes valeurs dedans, à partir du contenu de PaymentFormConfigurationRequest).
+        Et on va le renvoyer à Payline en lieu de place du formulaire de paiement
+        -> voir l'exemple de Google-Pay
+
+        Si on peut initialiser l'ordre directement dans le JS de paiement (sans avoir à faire un appel HTTP préalable),
+        c'est peut-être le mieux. Ca évite de générer un ordre pour rien si, au final, l'acheteur ne va pas au bout du
+        process de paiement.
+        -> Valider la faisabilité avec le script JS en local !
+        (Auquel cas, la méthode createOrder dans le client HTTP ne sert à rien... :/)
          */
 
         return null;
--- src/main/java/com/payline/payment/sharegroop/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/payline/payment/sharegroop/service/impl/PaymentServiceImpl.java
@@ -7,7 +7,15 @@ import com.payline.pmapi.service.PaymentService;
 public class PaymentServiceImpl implements PaymentService {
     @Override
     public PaymentResponse paymentRequest(PaymentRequest paymentRequest) {
-        // TODO
+
+        /* TODO: ici, on va avoir besoin de récupérer les infos qui ont été renvoyées par l'API Sharegroop au JS
+        lors du processus de paiement.
+        Il doit y avoir une callback Javascript du widget Payline à appeler à ce moment-là, dans le JS, pour sauvegarder les données de retour.
+        -> Creuser du côté de la constante JS_PARAM_VALUE_CALLBACK sur GooglePay...
+        -> Voir dans le service PaymentServiceImpl de GooglePay de quelle façon on récupère ces données
+        (de ce que je comprends, c'est dans le PaymentFormContext, avec la clé "data". Mais confirmer avec Jan.
+         */
+
         return null;
     }
 }
